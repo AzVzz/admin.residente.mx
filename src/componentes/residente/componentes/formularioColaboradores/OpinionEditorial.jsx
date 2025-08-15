6ab53b4a8fcc5a2fc4479715d45e7c147c310660
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-08-15 09:29:58 -0600

    hola

--- src/componentes/residente/componentes/formularioColaboradores/OpinionEditorial.jsx
+++ src/componentes/residente/componentes/formularioColaboradores/OpinionEditorial.jsx
@@ -7,9 +7,32 @@ import Nombre from "./componentes/Nombre"
 import OtrasRedesSociales from "./componentes/OtrasRedesSociales"
 import TextoOpinion from "./componentes/TextoOpinion"
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
 const OpinionEditorial = () => {
   return (
     <div className="flex flex-col gap-4">
+      <Box
+        component="form"
+        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Tu nombre" variant="outlined" />
+        <TextField id="outlined-basic" label="Correo electronico" variant="outlined" />
+        <TextField id="outlined-basic" label="Año de nacimiento" variant="outlined" />
+        <TextField id="outlined-basic" label="Lugar de nacimiento" variant="outlined" />
+        <TextField
+          id="outlined-multiline-static"
+          label="Curriculum"
+          multiline
+          rows={6}
+        />
+        <TextField id="outlined-basic" label="Opinión editorial" variant="outlined" />
+      </Box>
+
       <Nombre />
       <CorreoElectronico />
       <FechaNacimiento />
