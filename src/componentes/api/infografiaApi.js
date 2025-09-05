f30ea42503aac7357485c788295d68e816fbee7a
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-09-05 12:43:52 -0600

    infografia agregado para el formulario y en api

---
+++ src/componentes/api/infografiaApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://estrellasdenuevoleon.com.mx/api/infografias';
+
+// Obtener todas las infografías
+export const getInfografias = async () => {
+  const res = await axios.get(BASE_URL);
+  return res.data;
+};
+
+// Obtener la última infografía
+export const getUltimaInfografia = async () => {
+  const res = await axios.get(`${BASE_URL}/ultima`);
+  return res.data;
+};
+
+// Crear una nueva infografía (imagen y PDF)
+export const crearInfografia = async (formData) => {
+  const res = await axios.post(BASE_URL, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  });
+  return res.data;
+};
