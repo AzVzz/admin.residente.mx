9c870cc644ad99dca709faf83c9ef3317e382cf6
chris <christophervalero05@hotmail.com>
2025-09-09 12:12:47 -0600

    cambios de info

--- src/componentes/FooterPrincipal.jsx
+++ src/componentes/FooterPrincipal.jsx
@@ -4,12 +4,12 @@ import { urlApi } from './api/url';
 
 const FooterPrincipal = () => {
     return (
-        <footer className="bg-[#3b3b3c] text-white py-8">
+        <footer className="bg-[#3b3b3c] text-white py-8 mt-8">
             <div className="max-w-[1080px] mx-auto px-4">
                 {/* Primera fila: FoodDrinkMedia + iconos sociales */}
                 <div className="flex sm:flex-row justify-between items-center mb-4 sm:mb-8 gap-2">
                     <div className="flex items-center gap-3">
-                        <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/fooddrinkmedia-logo-blanco.webp`} alt="FoodDrinkMedia" className="w-full sm:h-13 object-contain" />
+                        <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/fooddrinkmedia-logo-blanco.webp`} alt="FoodDrinkMedia" className="w-full sm:h-10 object-contain" />
                     </div>
                     <div className="flex gap-2">
                         <a href="http://instagram.com/residentemty" target="_blank" rel="noopener noreferrer"><FaInstagram className="w-auto sm:h-7 h-4 text-white hover:text-gray-400" /></a>
@@ -20,11 +20,11 @@ const FooterPrincipal = () => {
                     </div>
                 </div>
                 {/* Segunda fila: Grid de 7 columnas */}
-                <div className="grid sm:grid-cols-7 grid-cols-4 gap-6 mb-8">
+                <div className="grid sm:grid-cols-7 grid-cols-4 gap-9 mb-8">
                     {/* Columna 1 */}
                     <div>
                         <h4 className="mb-3 text-[10px] sm:text-xl leading-3 sm:leading-4">Productos al consumidor</h4>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-5">
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/magazine-logo-blanco.webp`} alt="Magazine"
                                 className="sm:w-25 w-13 h-auto object-contain"
                             />
@@ -45,7 +45,7 @@ const FooterPrincipal = () => {
                     {/* Columna 2 */}
                     <div>
                         <h4 className="mb-3 text-[10px] sm:text-xl leading-3 sm:leading-4">Soluciones para la industria</h4>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-5">
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/newsletter-logo-blanco.webp`} alt="Newsletter"
                                 className="sm:w-25 w-13 h-auto object-contain"
                             />
@@ -60,7 +60,7 @@ const FooterPrincipal = () => {
                     {/* Columna 3 */}
                     <div>
                         <h4 className="mb-3 text-[10px] sm:text-xl leading-3 sm:leading-4">Proyectos culturales</h4>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-5">
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/platillos-logo-blanco.webp`} alt="Platillos" className="sm:w-25 w-13 h-auto object-contain" />
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/logo-estrellas-de-nl.webp`} alt="Estrellas de NL" className="sm:w-25 w-13 h-auto object-contain" />
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/rostros-logo-blanco.webp`} alt="Rostros" className="sm:w-25 w-13 h-auto object-contain" />
@@ -69,7 +69,7 @@ const FooterPrincipal = () => {
                     {/* Columna 4 */}
                     <div>
                         <h4 className="mb-3 text-[10px] sm:text-xl leading-3 sm:leading-4">Convenios institucionales</h4>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-5">
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/afirme-logo.webp`} alt="Afirme" className="sm:w-25 w-13 h-auto object-contain" />
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/canirac-logo.webp`} alt="Canirac" className="sm:w-25 w-13 h-auto object-contain" />
                             <img src={`${urlApi}fotos/fotos-estaticas/residente-logos/blancos/usda-blanco.webp`} alt="USDA" className="sm:w-25 w-13 h-auto object-contain" />
