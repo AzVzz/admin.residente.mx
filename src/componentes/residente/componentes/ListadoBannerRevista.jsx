6a45222376188b72e0e44a4bda05732d1f34d970
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-09-11 17:24:34 -0600

    imagen local host correjida

--- src/componentes/residente/componentes/ListadoBannerRevista.jsx
+++ src/componentes/residente/componentes/ListadoBannerRevista.jsx
@@ -215,7 +215,7 @@ const ListadoBannerRevista = ({
                                     <div className="absolute left-0 right-0 top-1/2 border-t-2 border-black opacity-100 z-0" />
                                     <div className="relative z-10 px-4 bg-[#DDDDDE]">
                                         <div className="flex flex-row justify-center items-center gap-3">
-                                            <img src={`http://localhost:3000/fotos/fotos-estaticas/residente-logos/negros/PLATILOS%20ICÓNICOS%20DE%20NL.webp`} className="w-full h-6 object-contain" />
+                                            <img src={`http://residente.mx/fotos/fotos-estaticas/residente-logos/negros/PLATILOS%20ICÓNICOS%20DE%20NL.webp`} className="w-full h-6 object-contain" />
                                         </div>
                                     </div>
                                 </div>
