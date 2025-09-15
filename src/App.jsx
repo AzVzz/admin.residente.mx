c4013ec6105fa3657ff862a5634c0eb2d4c2b37d
DiegoVzz <diegoazaelvazquez2016@gmail.com>
2025-09-15 11:07:19 -0600

    backup

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import BotonScroll from './componentes/residente/componentes/compFormularioMain/
 import Proximamente from './componentes/Proximamente.jsx';
 import ViewportAdjuster from './ViewportAdjuster.jsx';
 import InfografiaForm from './componentes/residente/infografia/InfografiaForm.jsx';
+import usePageTracking from './usePageTracking.js';
 
 
 //Admin
@@ -43,6 +44,7 @@ const FormularioRevistaBannerNueva = lazy(() => import('./componentes/residente/
 
 
 function App() {
+  usePageTracking();
 
   const location = useLocation();
   const [showMegaMenu, setShowMegaMenu] = useState(false);
