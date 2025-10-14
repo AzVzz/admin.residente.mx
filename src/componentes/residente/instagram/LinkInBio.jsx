6a1bc024192cf7b8238b18a2ffbadcd29eb1c102
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-10-14 17:20:21 -0600

    Cambios linkinbio links

--- src/componentes/residente/instagram/LinkInBio.jsx
+++ src/componentes/residente/instagram/LinkInBio.jsx
@@ -33,7 +33,7 @@ const LinkInBio = () => {
       <div className="flex items-center gap-2">
         {/* Logo de Residente (R en círculo negro) */}
                  <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center  ">
-           <Link to="/residente">
+           <Link to="/">
              <img 
                src={`${urlApi}/fotos/fotos-estaticas/residente-logos/negros/logo-r-residente-negro.webp`}
                alt="Logo Residente" 
@@ -46,7 +46,7 @@ const LinkInBio = () => {
   
       {/* Botón Ver Residente */}
       <button className="px-3 py-1 border border-gray-300 rounded text-gray-700 text-lg font-black hover:bg-gray-50 transition-colors">
-        <Link to="/residente">
+        <Link to="/">
           Ver Residente
         </Link>
       </button>
