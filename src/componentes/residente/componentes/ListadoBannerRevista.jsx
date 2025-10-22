714318e9767f771049b2fe3266d01be91b337fab
braxel29 <69501378+braxel29@users.noreply.github.com>
2025-10-22 10:20:44 -0600

    listado banner

--- src/componentes/residente/componentes/ListadoBannerRevista.jsx
+++ src/componentes/residente/componentes/ListadoBannerRevista.jsx
@@ -234,7 +234,7 @@ const ListadoBannerRevista = ({
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="pb-0">
+                                        <div className="pb-4">
                                             <CincoNotasBuscador
                                                 keywords={cfg.keyword}
                                                 limit={10}
