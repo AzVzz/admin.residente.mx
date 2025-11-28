6002bbd140ef246006b52e5f603d4f6f05dbbc9c
AzVzz <diegoazaelvazquez2016@gmail.com>
2025-11-28 13:33:34 -0600

    Formulario usuarios b2b

--- src/componentes/residente/componentes/compFormularioMain/ListaNotasUsuarios.jsx
+++ src/componentes/residente/componentes/compFormularioMain/ListaNotasUsuarios.jsx
@@ -399,7 +399,7 @@ const ListaNotasUsuarios = () => {
           className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded flex items-center"
         >
           <FaUserPlus className="mr-2" />
-          Nuevo Cliente
+          Nuevo Usuario
         </button>
       </div>
 
